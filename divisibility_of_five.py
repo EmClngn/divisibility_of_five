d54233f48c375608f5bb5a713ff946602b34a476
unknown <emgcalangian@gmail.com>
2024-01-22 15:38:43 +0800

    pseudo code

---
+++ divisibility_of_five.py
@@ -0,0 +1,7 @@
+# Iterate the given list of numbers and print only those numbers which are divisible by 5
+
+# Pseudo Code
+
+# ask user for number of elements they want in the list
+# make user input said elements
+# write the program that only prints numbers in a list that are divisbile by 5
