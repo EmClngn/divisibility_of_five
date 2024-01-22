54db17d6d8e627a9e073d9beb54a284cf0d7a719
unknown <emgcalangian@gmail.com>
2024-01-22 15:45:34 +0800

    asking user for elements

--- divisibility_of_five.py
+++ divisibility_of_five.py
@@ -3,5 +3,14 @@
 # Pseudo Code
 
 # ask user for number of elements they want in the list
+number_of_elements = int(input("How many elements do you want in the list? "))
+print(number_of_elements)
+
+list = []
 # make user input said elements
+for i in range(number_of_elements):
+    elements = input("Enter your desired element: ")
+    list.append(elements)
+
+print(list)
 # write the program that only prints numbers in a list that are divisbile by 5
